4b87d4324e99efd611233769db34501198d30122
Bazhanov Dmitri <bazhanovdmitro@gmail.com>
2020-10-21 16:27:18 +0300

    Cars related models comming right up

--- signup/admin.py
+++ signup/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Profile, userPreferences
+from .models import Profile, userPreferences, cars, car, carDetails
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(userPreferences)
+admin.site.register(cars)
+admin.site.register(car)
+admin.site.register(carDetails)
--- signup/models.py
+++ signup/models.py
@@ -4,12 +4,20 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+import uuid
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     company = models.BooleanField(default=False)
     email_confirmed = models.BooleanField(default=False)
+    companySpec = models.IntegerField(null=True, blank=True,
+        validators=[
+            MaxValueValidator(3),
+            MinValueValidator(1)
+        ])
 
 class userPreferences(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -22,12 +30,41 @@ class userPreferences(models.Model):
     frigeTemperature = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
 
+class cars(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # car.objects.filter(id).count()
+
+class car(models.Model):
+    cars = models.ForeignKey(cars, on_delete=models.CASCADE)
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular car")
+    mark = models.CharField(max_length=254)
+    model = models.CharField(max_length=254)
+    city = models.CharField(max_length=254)
+    county = models.CharField(max_length=254)
+    location = models.CharField(max_length=254)
+
+class carDetails(models.Model):
+    car = models.OneToOneField(car, on_delete=models.CASCADE)
+
+    radio = models.BooleanField(default=False)
+    display = models.BooleanField(default=False)
+    airConditioner = models.BooleanField(default=False)
+    glassFogging = models.BooleanField(default=False)
+    inside = models.CharField(max_length=255, default='dummy text')
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    lights = models.BooleanField(default=False) # Если True то ксенон. фары если False - обчные
+    colour = models.CharField(max_length=6, default='ffffff')
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
         userPreferences.objects.create(user=instance)
+        carsvar = cars.objects.create(user=instance)
+        carvar = car.objects.create(cars=carsvar)
+        carDetails.objects.create(car=carvar)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
